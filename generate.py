f04121b3e3f9a947c25fd4d19a4b2ca44fd69d59
Kaggy <kagnon@gmail.com>
2026-09-10 23:12:35 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -21,7 +21,6 @@ INCLUDE = {
     "community-day": "🌟",
     "pokemon-go-fest": "🎉",
     "raid-hour": "🚨",
-    "raid-battles": "🗡️", 
     "raid-day": "🗡️",
     "max-mondays": "🚨",
     "pokemon-spotlight-hour": "🔦",
